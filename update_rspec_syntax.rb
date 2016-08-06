9fbbb993e6d9bb891b9cc06a933d5a40a38ca469
Nick Phillips <nicktgphillips@gmail.com>
2016-08-05 18:11:56 -0700

    added 'should_not ==' clause fix

--- update_rspec_syntax.rb
+++ update_rspec_syntax.rb
@@ -12,6 +12,7 @@ File.foreach(filepath).with_index do |line, line_num|
   _line.sub!(/([^\s]*\w*)\.should_receive/, 'expect(\1).to receive')
   _line.sub!(/([^\s]*\w*)\.any_instance.should_not_receive/, 'expect_any_instance_of(\1).not_to receive')
   _line.sub!(/([^\s]*\w*)\.should_not_receive/, 'expect(\1).not_to receive')
+  _line.sub!(/([^\s]*\w*)\.should_not ==/, 'expect(\1).to_not eq')
   _line.sub!(/([^\s]*\w*)\.should ==/, 'expect(\1).to eq')
   _line.sub!(/([^\s]*\w*)\.should eq/, 'expect(\1).to eq')
   _line.sub!(/([^\s]*\w*)\.should/, 'expect(\1).to')
